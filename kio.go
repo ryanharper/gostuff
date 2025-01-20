448152cbbd76c182969f0f0fa28294dedde58718
Ryan Harper <ryanharper@users.noreply.github.com>
2025-01-20 17:48:53 +0000

    Update kio.go

--- kio.go
+++ kio.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+        "bytes"
+	"encoding/base64"
         "context"
         "fmt"
         "os"
 
         secretmanager "cloud.google.com/go/secretmanager/apiv1"
         "cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
-        "sigs.k8s.io/kustomize/kyaml/fn/framework"
-        "sigs.k8s.io/kustomize/kyaml/fn/framework/command"
         "sigs.k8s.io/kustomize/kyaml/kio"
         "sigs.k8s.io/kustomize/kyaml/yaml"
 )
@@ -22,6 +22,33 @@ type SecretGenerator struct {
                 SecretName     string            `yaml:"secretName"`
                 SecretMappings map[string]string `yaml:"secretMappings"`
         } `yaml:"spec"`
+}
+
+func (p *SecretGenerator) Filter(items []*yaml.RNode) ([]*yaml.RNode, error) {
+	var config *yaml.RNode
+	for i, item := range items {
+		if item.GetApiVersion() == "my.kustomize.plugins/v1" && item.GetKind() == "SecretGenerator" {
+			config = items[i]
+			items = append(items[:i], items[i+1:]...)
+			break
+		}
+	}
+
+	if config == nil {
+		return items, nil
+	}
+
+	if err := yaml.Unmarshal([]byte(config.MustString()), &p); err != nil {
+		return nil, err
+	}
+
+	generatedSecret, err := p.Generate()
+	if err != nil {
+		return nil, err
+	}
+
+	items = append(items, generatedSecret)
+	return items, nil
 }
 
 func (p *SecretGenerator) Generate() (*yaml.RNode, error) {
@@ -70,32 +97,19 @@ func fetchSecretFromGSM(ctx context.Context, client *secretmanager.Client, gsmSe
 }
 
 func base64Encode(input string) string {
-	return base64.StdEncoding.EncodeToString([]byte(input))
+        return base64.StdEncoding.EncodeToString([]byte(input))
 }
 
 func main() {
-        var p SecretGenerator
-        cmd := framework.Command(&p, func(items []*yaml.RNode) ([]*yaml.RNode, error) {
-                for i := range items {
-                        if items[i].GetApiVersion() == "my.kustomize.plugins/v1" && items[i].GetKind() == "SecretGenerator" {
-                                if err := yaml.Unmarshal([]byte(items[i].MustString()), &p); err != nil {
-                                        return nil, err
-                                }
+	p := &SecretGenerator{}
+	pipeline := kio.Pipeline{
+		Inputs:  []kio.Reader{&kio.ByteReader{Reader: os.Stdin}},
+		Filters: []kio.Filter{p},
+		Outputs: []kio.Writer{kio.ByteWriter{Writer: os.Stdout}},
+	}
 
-                                generatedSecret, err := p.Generate()
-                                if err != nil {
-                                        return nil, err
-                                }
-
-                                // Append the generated secret to the resource list
-                                items = append(items, generatedSecret)
-                        }
-                }
-                return items, nil
-        })
-
-        if err := cmd.Execute(); err != nil {
-                fmt.Fprintf(os.Stderr, "error: %v\n", err)
-                os.Exit(1)
-        }
+	if err := pipeline.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
 }
